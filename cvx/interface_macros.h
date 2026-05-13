e7d7376294b02452cfdfc5228e8448c3ee493fe3
Leonardo Vencovsky <lvenk26@gmail.com>
2026-05-13 11:54:15 +0200

    Add more interface macros

--- cvx/interface_macros.h
+++ cvx/interface_macros.h
@@ -13,8 +13,8 @@
 #define cvx_flag(ds) ((cvx_container *)(ds)->instance)->flag
 #define cvx_forward(ds, f) (ds)->vtable->forward((ds)->instance, f)
 #define cvx_index(ds) (ds)->vtable->index((ds)->instance)
-#define cvx_next(ds) (ds)->vtable->next((ds)->instance)
 #define cvx_new(ds) (ds)->vtable->new()
+#define cvx_next(ds) (ds)->vtable->next((ds)->instance)
 #define cvx_peek(ds) (ds)->vtable->peek((ds)->instance)
 #define cvx_peek_back(ds) (ds)->vtable->peek_back((ds)->instance)
 #define cvx_peek_front(ds) (ds)->vtable->peek_front((ds)->instance)
@@ -27,8 +27,9 @@
 #define cvx_push_front(ds, v) (ds)->vtable->push_front((ds)->instance, v)
 #define cvx_replace(ds, new) (ds)->vtable->replace((ds)->instance, new)
 #define cvx_start(ds) (ds)->vtable->start((ds)->instance)
-#define cvx_to_start(ds) (ds)->vtable->to_start((ds)->instance)
+#define cvx_tag(ds) ((cvx_container *)(ds)->instance)->tag
 #define cvx_to_end(ds) (ds)->vtable->to_end((ds)->instance)
+#define cvx_to_start(ds) (ds)->vtable->to_start((ds)->instance)
 #define cvx_value(ds) (ds)->vtable->value((ds)->instance)
 
 #endif /* CVX_HELPER_MACROS_H */
